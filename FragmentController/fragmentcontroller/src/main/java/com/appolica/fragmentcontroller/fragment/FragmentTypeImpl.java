037b4db09112c091d449fb578bebb4d843981608
Antonio Mindov <amindov@appolica.com>
2017-08-07 12:10:59 +0300

    Added arguments for root fragment

--- FragmentController/fragmentcontroller/src/main/java/com/appolica/fragmentcontroller/fragment/FragmentTypeImpl.java
+++ FragmentController/fragmentcontroller/src/main/java/com/appolica/fragmentcontroller/fragment/FragmentTypeImpl.java
@@ -1,25 +1,34 @@
 package com.appolica.fragmentcontroller.fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 public class FragmentTypeImpl implements ControllerFragmentType {
 
     private final Class<? extends Fragment> frClass;
     private String tag;
+    private Bundle args;
 
     public FragmentTypeImpl(Class<? extends Fragment> frClass) {
         this(frClass, frClass.getName());
     }
 
     public FragmentTypeImpl(Class<? extends Fragment> frClass, String tag) {
+        this(frClass, tag, null);
+    }
+
+    public FragmentTypeImpl(Class<? extends Fragment> frClass, String tag, Bundle args) {
         this.frClass = frClass;
         this.tag = tag;
+        this.args = args;
     }
 
     @Override
     public Fragment getInstance() {
         try {
-            return frClass.newInstance();
+            Fragment fragment = frClass.newInstance();
+            fragment.setArguments(args);
+            return fragment;
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
